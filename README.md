b331431f9d110c279f13bebc7cc7a4140844d558
talkingscott <talkingscott@gmail.com>
2016-07-13 20:26:53 -0400

    Configure through environment

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # s3imageserver
 A simple web server for images in S3 that supports resizing (scaling)
+
+## Invoking
+
+The URL pattern is /2/image/:image/width/:width
+
+## Configuration
+
+Application configuration is through the environment.
+
+```
+export IMAGEBUCKET=image-bucket-name
+export IMAGEBUCKETREGION=us-east-1
+```
+
+AWS configuration is AWS SDK for go standard, namely ~/.aws/credentials for credentials.
--- s3imageserver.go
+++ s3imageserver.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"fmt"
+	"image"
+	_ "image/gif"
 	"image/jpeg"
+	_ "image/png"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
 
@@ -14,9 +19,14 @@ import (
 )
 
 func main() {
-	svc := s3.New(session.New(&aws.Config{Region: aws.String("us-east-1")}))
-		
+	region := os.Getenv("IMAGEBUCKETREGION")
+	bucket := os.Getenv("IMAGEBUCKET")
+	fmt.Fprintf(os.Stdout, "Region: %s bucket: %s\n", region, bucket)
+
+	svc := s3.New(session.New(&aws.Config{Region: aws.String(region)}))
+
 	http.HandleFunc("/2/", func(w http.ResponseWriter, r *http.Request) {
+		// parse URL
 		pieces := strings.Split(r.URL.Path, "/")
 		if len(pieces) != 6 || pieces[2] != "image" || pieces[4] != "width" {
 			http.NotFound(w, r)
@@ -29,10 +39,11 @@ func main() {
 			return
 		}
 
-		// if If-Modified-Since header is present, compare to file date and return 304 if unmodified
+		// get image from bucket
+		// TODO: if If-Modified-Since header is present, compare to object date and return 304 if unmodified
 		params := &s3.GetObjectInput{
-			Bucket:                     aws.String("com.scottnichol.s3test"), // Required
-			Key:                        aws.String(pieces[3]),  // Required
+			Bucket: aws.String(bucket),
+			Key: aws.String(pieces[3]),
 		}
 		resp, err := svc.GetObject(params)
 		
@@ -41,15 +52,14 @@ func main() {
 			return
 		}
 		
-		// decode jpeg into image.Image
-		img, err := jpeg.Decode(resp.Body)
+		// decode image
+		img, _, err := image.Decode(resp.Body)
 		if err != nil {
 			http.Error(w, err.Error(), 500)
 			return
 		}
 
-		// resize to width using Lanczos resampling
-		// and preserve aspect ratio
+		// resize to width using Lanczos resampling and preserve aspect ratio
 		m := resize.Resize(uint(width), 0, img, resize.Lanczos3)
 
 		// set headers
@@ -57,10 +67,9 @@ func main() {
 		w.Header().Set("cache-control", "max-age=86400")
 		// TODO: set last-modified to file modified date
 
-    		// write new image 
+    	// encode image and stream as response
 		jpeg.Encode(w, m, nil)
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
-
