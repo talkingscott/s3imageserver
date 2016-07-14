12ddd2850334de61db9f791f7704f262c53ceda6
talkingscott <talkingscott@gmail.com>
2016-07-13 22:16:41 -0400

    Support If-Modified-Since.  Check request method.  Use SDK AWS_REGION standard.

--- README.md
+++ README.md
@@ -7,11 +7,13 @@ The URL pattern is /2/image/:image/width/:width
 
 ## Configuration
 
-Application configuration is through the environment.
+AWS configuration is AWS SDK for go standard, namely ~/.aws/credentials for credentials
+and environment variable AWS_REGION for region.
+
+Application configuration is through the environment.  So you probably would have something like
 
 ```
 export IMAGEBUCKET=image-bucket-name
-export IMAGEBUCKETREGION=us-east-1
+export AWS_REGION=us-east-1
 ```
 
-AWS configuration is AWS SDK for go standard, namely ~/.aws/credentials for credentials.
--- s3imageserver.go
+++ s3imageserver.go
@@ -11,21 +11,32 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/nfnt/resize"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 func main() {
-	region := os.Getenv("IMAGEBUCKETREGION")
 	bucket := os.Getenv("IMAGEBUCKET")
-	fmt.Fprintf(os.Stdout, "Region: %s bucket: %s\n", region, bucket)
+	fmt.Fprintf(os.Stdout, "Bucket: %s\n", bucket)
 
-	svc := s3.New(session.New(&aws.Config{Region: aws.String(region)}))
+	// let SDK set region via AWS_REGION
+	//config := aws.Config{}
+	//config.WithLogLevel(aws.LogDebugWithHTTPBody)
+	//svc := s3.New(session.New(&config))
+	svc := s3.New(session.New(&aws.Config{}))
 
 	http.HandleFunc("/2/", func(w http.ResponseWriter, r *http.Request) {
+		// TODO: support HEAD
+		if r.Method != "GET" {
+			http.Error(w, "", http.StatusMethodNotAllowed)
+			return
+		}
+
 		// parse URL
 		pieces := strings.Split(r.URL.Path, "/")
 		if len(pieces) != 6 || pieces[2] != "image" || pieces[4] != "width" {
@@ -39,16 +50,46 @@ func main() {
 			return
 		}
 
+		ifModifiedSince := r.Header.Get("if-modified-since")
+		fmt.Fprintf(os.Stdout, "if-modified-since: %s\n", ifModifiedSince)
+
 		// get image from bucket
-		// TODO: if If-Modified-Since header is present, compare to object date and return 304 if unmodified
-		params := &s3.GetObjectInput{
+		params := s3.GetObjectInput{
 			Bucket: aws.String(bucket),
 			Key: aws.String(pieces[3]),
 		}
-		resp, err := svc.GetObject(params)
+		if ifModifiedSince != "" {
+			tm, err := time.Parse(time.RFC1123, ifModifiedSince)
+			if err != nil {
+				fmt.Fprintf(os.Stdout, "parse error %s\n", err)
+			} else {
+				params.IfModifiedSince = aws.Time(tm)
+				fmt.Fprintf(os.Stdout, "set IfModifiedSince to %s\n", params.IfModifiedSince.Format(time.RFC1123))
+			}
+		}
+		resp, err := svc.GetObject(&params)
 		
 		if err != nil {
-			http.Error(w, err.Error(), 404)
+			if awsErr, ok := err.(awserr.Error); ok {
+				// Get error details
+				fmt.Fprintf(os.Stdout, "AWS Code: %s Message: %s\n", awsErr.Code(), awsErr.Message())
+
+				// Prints out full error message, including original error if there was one.
+				fmt.Fprintf(os.Stdout, "AWS Error: %s\n", awsErr.Error())
+		 
+				// Get original error
+				if origErr := awsErr.OrigErr(); origErr != nil {
+					// operate on original error.
+				}
+				if awsErr.Code() == "304NotModified" {
+					http.Error(w, awsErr.Message(), 304)
+				} else {
+					http.Error(w, awsErr.Message(), 404)
+				}
+			} else {
+				fmt.Fprintf(os.Stdout, "Plain error: %s\n", err.Error())
+				http.Error(w, err.Error(), 404)
+			}
 			return
 		}
 		
@@ -65,9 +106,9 @@ func main() {
 		// set headers
 		w.Header().Set("content-type", "image/jpeg")
 		w.Header().Set("cache-control", "max-age=86400")
-		// TODO: set last-modified to file modified date
+		w.Header().Set("last-modified", resp.LastModified.Format(time.RFC1123))
 
-    	// encode image and stream as response
+		// encode image and stream as response
 		jpeg.Encode(w, m, nil)
 	})
 
