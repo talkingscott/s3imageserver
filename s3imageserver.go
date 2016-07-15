16c8421d5f990979439929f7495b748e2d73c6f7
talkingscott <talkingscott@gmail.com>
2016-07-14 23:12:17 -0400

    Log timings.  Put handler in a separate func.

--- s3imageserver.go
+++ s3imageserver.go
@@ -20,97 +20,118 @@ import (
 	"github.com/aws/aws-sdk-go/service/s3"
 )
 
+var bucket string
+var svc *s3.S3
+
+func imageHandler(w http.ResponseWriter, r *http.Request) {
+	// TODO: support HEAD
+	if r.Method != "GET" {
+		http.Error(w, "", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// parse URL
+	pieces := strings.Split(r.URL.Path, "/")
+	if len(pieces) != 6 || pieces[2] != "image" || pieces[4] != "width" {
+		http.NotFound(w, r)
+		return
+	}
+
+	width, err := strconv.ParseUint(pieces[5], 10, 32);
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	ifModifiedSince := r.Header.Get("if-modified-since")
+	fmt.Fprintf(os.Stdout, "Got if-modified-since: %s\n", ifModifiedSince)
+
+	// get image from bucket
+	params := s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key: aws.String(pieces[3]),
+	}
+	if ifModifiedSince != "" {
+		tm, err := time.Parse(time.RFC1123, ifModifiedSince)
+		if err != nil {
+			fmt.Fprintf(os.Stdout, "Parse error %s\n", err)
+		} else {
+			params.IfModifiedSince = aws.Time(tm)
+			fmt.Fprintf(os.Stdout, "Set IfModifiedSince to %s\n", params.IfModifiedSince.Format(time.RFC1123))
+		}
+	}
+	
+	start := time.Now()
+	resp, err := svc.GetObject(&params)
+	
+	if err != nil {
+		if awsErr, ok := err.(awserr.Error); ok {
+			// Get error details
+			fmt.Fprintf(os.Stdout, "AWS Code: %s Message: %s\n", awsErr.Code(), awsErr.Message())
+
+			// Prints out full error message, including original error if there was one.
+			fmt.Fprintf(os.Stdout, "AWS Error: %s\n", awsErr.Error())
+	 
+			// Get original error
+			if origErr := awsErr.OrigErr(); origErr != nil {
+				// operate on original error.
+			}
+			if awsErr.Code() == "304NotModified" {
+				http.Error(w, awsErr.Message(), 304)
+			} else {
+				http.Error(w, awsErr.Message(), 404)
+			}
+		} else {
+			fmt.Fprintf(os.Stdout, "Plain error: %s\n", err.Error())
+			http.Error(w, err.Error(), 404)
+		}
+		return
+	}
+
+	getDuration := time.Since(start)
+	start = time.Now()
+
+	// decode image
+	img, _, err := image.Decode(resp.Body)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	decodeDuration := time.Since(start)
+	start = time.Now()
+
+	// resize to width using Lanczos resampling and preserve aspect ratio
+	m := resize.Resize(uint(width), 0, img, resize.Lanczos3)
+
+	resizeDuration := time.Since(start)
+
+	// set headers
+	w.Header().Set("content-type", "image/jpeg")
+	w.Header().Set("cache-control", "max-age=86400")
+	w.Header().Set("last-modified", resp.LastModified.Format(time.RFC1123))
+
+	start = time.Now()
+
+	// encode image and stream as response
+	jpeg.Encode(w, m, nil)
+	
+	encodeDuration := time.Since(start)
+	
+	fmt.Fprintf(os.Stdout, "%s get: %f decode: %f resize: %f encode: %f\n", r.URL.Path, getDuration.Seconds(), decodeDuration.Seconds(), resizeDuration.Seconds(), encodeDuration.Seconds())
+}
+
 func main() {
-	bucket := os.Getenv("IMAGEBUCKET")
+	bucket = os.Getenv("IMAGEBUCKET")
 	fmt.Fprintf(os.Stdout, "Bucket: %s\n", bucket)
 
 	// let SDK set region via AWS_REGION
 	//config := aws.Config{}
 	//config.WithLogLevel(aws.LogDebugWithHTTPBody)
 	//svc := s3.New(session.New(&config))
-	svc := s3.New(session.New(&aws.Config{}))
+	svc = s3.New(session.New(&aws.Config{}))
 
-	http.HandleFunc("/2/", func(w http.ResponseWriter, r *http.Request) {
-		// TODO: support HEAD
-		if r.Method != "GET" {
-			http.Error(w, "", http.StatusMethodNotAllowed)
-			return
-		}
-
-		// parse URL
-		pieces := strings.Split(r.URL.Path, "/")
-		if len(pieces) != 6 || pieces[2] != "image" || pieces[4] != "width" {
-			http.NotFound(w, r)
-			return
-		}
-
-		width, err := strconv.ParseUint(pieces[5], 10, 32);
-		if err != nil {
-			http.Error(w, err.Error(), 500)
-			return
-		}
-
-		ifModifiedSince := r.Header.Get("if-modified-since")
-		fmt.Fprintf(os.Stdout, "if-modified-since: %s\n", ifModifiedSince)
-
-		// get image from bucket
-		params := s3.GetObjectInput{
-			Bucket: aws.String(bucket),
-			Key: aws.String(pieces[3]),
-		}
-		if ifModifiedSince != "" {
-			tm, err := time.Parse(time.RFC1123, ifModifiedSince)
-			if err != nil {
-				fmt.Fprintf(os.Stdout, "parse error %s\n", err)
-			} else {
-				params.IfModifiedSince = aws.Time(tm)
-				fmt.Fprintf(os.Stdout, "set IfModifiedSince to %s\n", params.IfModifiedSince.Format(time.RFC1123))
-			}
-		}
-		resp, err := svc.GetObject(&params)
-		
-		if err != nil {
-			if awsErr, ok := err.(awserr.Error); ok {
-				// Get error details
-				fmt.Fprintf(os.Stdout, "AWS Code: %s Message: %s\n", awsErr.Code(), awsErr.Message())
-
-				// Prints out full error message, including original error if there was one.
-				fmt.Fprintf(os.Stdout, "AWS Error: %s\n", awsErr.Error())
-		 
-				// Get original error
-				if origErr := awsErr.OrigErr(); origErr != nil {
-					// operate on original error.
-				}
-				if awsErr.Code() == "304NotModified" {
-					http.Error(w, awsErr.Message(), 304)
-				} else {
-					http.Error(w, awsErr.Message(), 404)
-				}
-			} else {
-				fmt.Fprintf(os.Stdout, "Plain error: %s\n", err.Error())
-				http.Error(w, err.Error(), 404)
-			}
-			return
-		}
-		
-		// decode image
-		img, _, err := image.Decode(resp.Body)
-		if err != nil {
-			http.Error(w, err.Error(), 500)
-			return
-		}
-
-		// resize to width using Lanczos resampling and preserve aspect ratio
-		m := resize.Resize(uint(width), 0, img, resize.Lanczos3)
-
-		// set headers
-		w.Header().Set("content-type", "image/jpeg")
-		w.Header().Set("cache-control", "max-age=86400")
-		w.Header().Set("last-modified", resp.LastModified.Format(time.RFC1123))
-
-		// encode image and stream as response
-		jpeg.Encode(w, m, nil)
-	})
+	http.HandleFunc("/2/", imageHandler)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
